81b1210a00ed234bb375202c08c9b7a2b5b0b574
abc <abc@gmail.com>
2025-10-08 12:57:58 +0530

    Final update

--- src/pages/SchoolsPage.jsx
+++ src/pages/SchoolsPage.jsx
@@ -5,11 +5,11 @@ import DirectoryPage from "../components/DirectoryPage";
 export default function SchoolsPage() {
   const [school, setSchool] = useState([])
 
-  useEffect(()=> {
-    useEffect(() => {
+  useEffect(() => {
     document.title = "Thiruvarur District Schools | Education Details";
   }, []);
 
+  useEffect(()=> {
     fetch('https://api.jsonbin.io/v3/b/68d84250ae596e708ffdfd41')
       .then((res) => res.json())
       // .then((res) => console.log(res.record.schools))
